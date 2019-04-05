cdcaa63b29c77eed9b52b060c0881724bb62d578
Allan Pichardo <allan.pichardo@gmail.com>
2019-04-05 00:01:40 -0400

    benchmarking

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "Scene.h"
 
 int main() {
-    Scene scene("../scenes/scene1.txt");
+    Scene scene("../scenes/scene5.txt");
     if(scene.isSceneLoaded()) {
         scene.renderToImage("../output/render.bmp");
     }
